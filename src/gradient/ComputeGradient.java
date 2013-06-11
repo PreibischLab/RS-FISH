6e9dec09e403c7737977f7712805818248874888
Stephan Preibisch <stephan.preibisch@gmx.de>
2013-06-10 21:57:33 -0400

    added a layer of abstraction so that gradient also works in any dimensionality now

--- src/gradient/ComputeGradient.java
+++ src/gradient/ComputeGradient.java
@@ -1,17 +1,9 @@
 package gradient;
 
-import net.imglib2.EuclideanSpace;
 import net.imglib2.Localizable;
 
-public abstract class Gradient implements EuclideanSpace
+public interface ComputeGradient 
 {
-	final int numDimensions;
-	
-	public Gradient( final int numDimensions )
-	{
-		this.numDimensions = numDimensions;
-	}
-	
 	/**
 	 * Computes the n-dimensional 1st derivative vector in center of a 2x2x2...x2 environment for a certain location
 	 * defined by the position of the RandomAccess
@@ -19,8 +11,5 @@ public abstract class Gradient implements EuclideanSpace
 	 * @param location - the top-left-front position for which to compute the derivative
 	 * @param derivativeVector - where to put the derivative vector [3]
 	 */
-	public abstract void gradientAt( final Localizable location, final float[] derivativeVector );
-	
-	@Override
-	public int numDimensions() { return numDimensions; }
+	public void gradientAt( final Localizable location, final float[] derivativeVector );
 }

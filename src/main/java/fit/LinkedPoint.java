9e380a0d8cbe0b06a06a62cfd29833cce1b91a66
Stephan Preibisch <stephan.preibisch@gmx.de>
2015-09-22 11:07:40 +0200

    added license

--- src/main/java/fit/LinkedPoint.java
+++ src/main/java/fit/LinkedPoint.java
@@ -3,6 +3,24 @@ package fit;
 import net.imglib2.util.Util;
 import mpicbg.models.Point;
 
+/**
+ * Radial Symmetry Package
+ *
+ * This software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Stephan Preibisch (stephan.preibisch@gmx.de) & Timothee Lionnet
+ */
 public class LinkedPoint<P> extends Point
 {
 	private static final long serialVersionUID = 1L;

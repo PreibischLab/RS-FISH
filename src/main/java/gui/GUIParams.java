1d5832e36eaa99515779ecaeecba4ec8e17483e8
Stephan Preibisch <stephan.preibisch@gmx.de>
2017-03-10 12:18:41 +0100

    added an interface for the parameters that should come out of the GUI

---
+++ src/main/java/gui/GUIParams.java
@@ -0,0 +1,17 @@
+package gui;
+
+public interface GUIParams
+{
+	public float sigmaDoG();
+	public float thresholdDoG();
+
+	public int bsMethod(); //  "No background subtraction", "Mean", "Median", "RANSAC on Mean", "RANSAC on Median" };
+	public float bsMaxErrorRANSAC();
+	public float bsInlierRatioRANSAC();
+
+	public float maxErrorRANSAC();
+	public float inlierRationRANSAC();
+	public int supportRadiusRANSAC();
+
+	
+}

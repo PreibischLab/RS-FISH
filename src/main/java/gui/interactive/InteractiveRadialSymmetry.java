0f59721a5a2a5f24a416eafb10218ea03787cd8c
milkyklim <klim.kolyvanov@gmail.com>
2017-04-03 12:32:26 +0200

    removed commented code

--- src/main/java/gui/interactive/InteractiveRadialSymmetry.java
+++ src/main/java/gui/interactive/InteractiveRadialSymmetry.java
@@ -236,54 +236,8 @@ public class InteractiveRadialSymmetry// extends GUIParams
 	 * */
 	protected void initParameters( final ImagePlus imp )
 	{
-	
 		// 2 because you want to calibrate only XY-plane
 		this.calibration = HelperFunctions.initCalibration(imp, 2);
-
-//		try
-//		{
-//			final Calibration cal = imp.getCalibration();
-//	
-//			if (
-//					cal == null ||
-//					Double.isNaN( cal.pixelWidth ) ||
-//					Double.isNaN( cal.pixelHeight ) ||
-//					Double.isInfinite( cal.pixelWidth ) ||
-//					Double.isInfinite( cal.pixelHeight ) ||
-//					cal.pixelHeight <= 0 ||
-//					cal.pixelHeight <= 0 ||
-//					cal.pixelWidth == cal.pixelHeight
-//					)
-//			{
-//				this.calibration = new double[]{ 1, 1 };
-//			}
-//			else
-//			{
-//				IJ.log( "WARNING: Pixel calibration is not symmetric in XY! Please check this (Image > Properties)" );
-//				IJ.log( "x: " + cal.pixelWidth ); 
-//				IJ.log( "y: " + cal.pixelHeight );
-//	
-//				if ( cal.pixelWidth < cal.pixelHeight ) // x has a higher resolution than y
-//					this.calibration = new double[]{ 1, cal.pixelHeight / cal.pixelWidth };
-//				else
-//					this.calibration = new double[]{ cal.pixelHeight / cal.pixelWidth, 1 };
-//			}
-//		}
-//		catch ( Exception e ) { this.calibration = new double[]{ 1, 1 }; }
-//
-//		IJ.log( "Using relative [x, y] calibration: " + Util.printCoordinates( this.calibration ) );
-	
-		// these values are initialized with defaults
-//		sigma = defaultSigma;
-//		threshold = defaultThreshold;
-//
-//		maxError = defaultMaxError;
-//		inlierRatio = defaultInlierRatio;
-//		supportRadius = defaultSupportRadius;
-//
-//		bsInlierRatio = defaultBSInlierRatio;
-//		bsMaxError = defaultBSMaxError;
-//		bsMethod = defaultBSMethod;
 	}
 
 

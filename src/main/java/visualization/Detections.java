4a8810404e387455e2c09f0a45565f22f9a3dccf
milkyklim <klim.kolyvanov@gmail.com>
2017-08-14 17:13:01 +0200

    added functions to show the detected blobs

--- src/main/java/visualization/Detections.java
+++ src/main/java/visualization/Detections.java
@@ -1,5 +1,18 @@
 package visualization;
 
+import java.util.ArrayList;
+
+import fit.Spot;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.type.numeric.real.FloatType;
+
 public class Detections {
 
+	public static void showDetections(RandomAccessibleInterval <FloatType> img, ArrayList<Spot> spot){
+		
+	}
+	
+	public static void main(String [] args){
+		
+	}
 }

c946d88503ff593d151f4e2c0d8260ce4dffdb9b
milkyklim <klim.kolyvanov@gmail.com>
2017-08-17 11:42:43 +0200

    fixed the problem with different channels/stacks/timepoints

--- src/main/java/visualization/Inliers.java
+++ src/main/java/visualization/Inliers.java
@@ -3,7 +3,10 @@ package visualization;
 import java.util.ArrayList;
 
 import fit.Spot;
+import ij.CompositeImage;
 import ij.ImagePlus;
+import ij.ImageStack;
+
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
@@ -34,12 +37,33 @@ public class Inliers {
 				
 		// TODO: for the images with time have to make a loop
 		
+		final boolean encodeErrors = false;
 		Img<FloatType> imgInliers = new ArrayImgFactory<FloatType>().create(dimensions, new FloatType());
-		Spot.drawRANSACArea(spots, imgInliers);
+		Spot.drawRANSACArea(spots, imgInliers, encodeErrors);
 		// TODO: (re)-move 
 		ImageJFunctions.show(imgInliers).setTitle("RANSAC inliers");
 		
-		mergeImages(img, imgInliers);
+		final ImagePlus imp = ImageJFunctions.wrap(img, "input" ).duplicate();
+		final ImagePlus impInliers = ImageJFunctions.wrap(imgInliers, "inliers" ).duplicate();
+
+		final ImageStack stack = new ImageStack(imp.getWidth(), imp.getHeight() );
+		
+		for ( int i = 0; i < imp.getStackSize(); ++i )
+		{
+			stack.addSlice( imp.getStack().getProcessor( i + 1 ) );
+			stack.addSlice( impInliers.getStack().getProcessor( i + 1 ) );
+		}
+		
+		ImagePlus merged = new ImagePlus("merge", stack );
+		merged.setDimensions( 2, imp.getStack().getSize(), 1 );
+		
+		CompositeImage ci = new CompositeImage( merged );
+		ci.setDisplayMode( CompositeImage.COMPOSITE );
+		//ci.setSlice( imp.getStackSize() / 2 );
+		//ci.resetDisplayRange();
+		ci.show();
+
+		//mergeImages(img, imgInliers);
 	}
 	
 	

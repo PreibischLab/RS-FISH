d1e32b231feb9f1d35082f1a64edfefbe368878c
milkyklim <klim.kolyvanov@gmail.com>
2017-08-19 18:02:48 +0200

    stable points test is here

--- src/main/java/stable/points/StablePoints.java
+++ src/main/java/stable/points/StablePoints.java
@@ -4,30 +4,17 @@ import java.io.File;
 import java.util.ArrayList;
 
 import net.imglib2.Cursor;
-import net.imglib2.Point;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.dog.DogDetection;
-import net.imglib2.algorithm.localextrema.RefinedPeak;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 
 import background.NormalizedGradient;
-import background.NormalizedGradientAverage;
-import background.NormalizedGradientMedian;
-import background.NormalizedGradientRANSAC;
 import fit.Spot;
-import fit.Center.CenterMethod;
 import gradient.Gradient;
 import gradient.GradientPreCompute;
-import gui.Radial_Symmetry;
-import gui.interactive.HelperFunctions;
-import ij.IJ;
 import ij.ImageJ;
-import parameters.RadialSymmetryParameters;
 import util.ImgLib2Util;
 
 public class StablePoints {
@@ -42,18 +29,15 @@ public class StablePoints {
 		// set the initital points
 		while (cursor.hasNext()){
 			cursor.fwd();
-			long [] position = new long[numDimensions]; // should be created inside of the loop, stupid java thing			
+			// should be created inside of the loop, stupid java thing	
+			long [] position = new long[numDimensions]; 		
 			cursor.localize(position);
 			simplifiedPeaks.add(position);
 		}
 		
-		//simplifiedPeaks.add(new long[]{50,50,50});
-		
 		System.out.println("# of peaks: " + simplifiedPeaks.size());
 		Gradient derivative = new GradientPreCompute(img);	
-		
-		// ImageJFunctions.show(new GradientPreCompute(img).preCompute(img));
-		
+		// do not normalize the gradient 
 		final NormalizedGradient ng = null;
 		
 		final long[] range = new long[numDimensions];
@@ -62,37 +46,37 @@ public class StablePoints {
 		
 		ArrayList<Spot> spots = Spot.extractSpots(img, simplifiedPeaks, derivative, ng, range);
 		System.out.println("# of spots: " + spots.size());
+		// stores the coordinates of the spots that didn't throw the exception 
+		ArrayList<double[]> goodSpots = new ArrayList<>(); 
 		
 		for (final Spot spot : spots){
 			try{
-				// 50 50 50
-				// System.out.println("GOOD!");
-				Spot.fitCandidates(spot);
-				
-//				for (int d = 0; d < numDimensions; d++)
-//					System.out.print(spot.getLongPosition(d) + " ");
-//				System.out.println();
-//								
-//				if (spot.getLongPosition(0) == 50 &&
-//					spot.getLongPosition(1) == 50 &&
-//					spot.getLongPosition(2) == 50)
-//						System.out.println("PING!");
-				
-				System.out.println("GOOD!");
+				// real coordinate: 50 50 50
+				Spot.fitCandidates(spot);				
+				goodSpots.add(spot.getCenter());
 			}
 			catch(Exception e){
 				// System.out.println("EXCEPTION CAUGHT");
 			}
 		}	
 		
-		// for 
+		for (final double[] goodSpot : goodSpots){
+			if (Spot.length(new double[]{goodSpot[0] - 50, goodSpot[1] - 50, goodSpot[2] - 50}) < 0.5){				
+				System.out.print(Spot.length(new double[]{goodSpot[0] - 50, goodSpot[1] - 50, goodSpot[2] - 50}) + ": ");
+				for (int d = 0; d < numDimensions; d++)
+					System.out.print(goodSpot[d] + " ");
+				System.out.println();
+			}
+		}
+		
+		
 	}
 
 	
 	public static void main(String[] args) {
 
 		new ImageJ();
-		File file = new File("/Users/kkolyva/Desktop/untitled folder/gauss3d-1,2,3.tif"); 
+		File file = new File("/Users/kkolyva/Desktop/untitled folder/one-spot.tif"); 
 		Img<FloatType> img = ImgLib2Util.openAs32Bit(file);
 
 		ImageJFunctions.show(img);

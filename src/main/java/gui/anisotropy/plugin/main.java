9e0a0af7e089a0f940a54d529211d7239fba7a21
milkyklim <klim.kolyvanov@gmail.com>
2017-09-27 17:16:56 +0200

    LUL! [2] Added classes to trigger plugins

---
+++ src/main/java/gui/anisotropy/plugin/main.java
@@ -0,0 +1,33 @@
+package gui.anisotropy.plugin;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.imagej.Dataset;
+
+public class main {
+	
+	public static void main(String[] args)
+	{
+		File path = new File("/Users/kkolyva/Desktop/2017-09-20-hackathon-dresden-projects/2017-09-20-anisotropy-fix/beads-cropped.tif");
+
+		// create the ImageJ application context with all available services
+		final net.imagej.ImageJ ij = new net.imagej.ImageJ();
+		ij.ui().showUI();
+
+		// load the dataset
+		Dataset dataset;
+		try {
+			dataset = ij.scifio().datasetIO().open(path.getAbsolutePath());
+			// show the image
+			ij.ui().show(dataset);
+			// invoke the plugin
+			ij.command().run(Anisotropy_Plugin.class, true);
+
+		} catch (IOException exc) {
+			System.out.println("LUL!");
+		}
+	
+		System.out.println("DOGE!");
+	}
+}
---
+++ src/main/java/gui/main.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.imagej.Dataset;
+
+/*
+ * 
+ * New main class for testing, SUCKS
+ * */
+
+public class main {
+
+	public static void main(String[] args) {
+		File path = new File( "/Users/kkolyva/Desktop/2017-09-20-hackathon-dresden-projects/2017-09-20-anisotropy-fix/Simulated_3D_2x.tif" );
+		File path2 = new File( "/Users/kkolyva/Desktop/2017-09-20-hackathon-dresden-projects/2017-09-20-anisotropy-fix/Simulated_3D_4x.tif" );
+		// create the ImageJ application context with all available services
+		final net.imagej.ImageJ ij = new net.imagej.ImageJ();
+		ij.ui().showUI();
+
+		// load the dataset
+		Dataset dataset;
+		Dataset dataset2;
+		try {
+			dataset = ij.scifio().datasetIO().open(path.getAbsolutePath());
+			dataset2 = ij.scifio().datasetIO().open(path2.getAbsolutePath());
+			
+			// show the image
+			ij.ui().show(dataset);
+			ij.ui().show(dataset2);
+			// invoke the plugin
+			ij.command().run(Radial_Symmetry.class, true);
+
+		} catch (IOException exc) {
+			System.out.println("LUL!");
+		}
+		
+		System.out.println("Doge!");
+	}
+}

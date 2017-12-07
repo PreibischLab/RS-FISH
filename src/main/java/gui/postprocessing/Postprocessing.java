fe47db5a769da0f9be6fbba1496221e67e583ee8
milkyklim <klim.kolyvanov@gmail.com>
2017-12-07 11:56:44 +0100

    changed the type of the parameters

--- src/main/java/gui/postprocessing/Postprocessing.java
+++ src/main/java/gui/postprocessing/Postprocessing.java
@@ -1,9 +1,12 @@
 package gui.postprocessing;
 
+import java.io.File;
+
 import org.scijava.command.Command;
 import org.scijava.command.ContextCommand;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.widget.FileWidget;
 
 import ij.ImagePlus;
 
@@ -13,11 +16,11 @@ public class Postprocessing extends ContextCommand{
 	@Parameter(autoFill = false, label = "Nuclei mask:")
 	ImagePlus imp;
 	
-	@Parameter(label = "Intron results:")
-	String intronPath;
+	@Parameter(label = "Intron results:", style=FileWidget.OPEN_STYLE)
+	File intronPath;
 	
-	@Parameter(label = "Exon results:")
-	String exonPath;
+	@Parameter(label = "Exon results:", style=FileWidget.OPEN_STYLE)
+	File exonPath;
 	
 	@Override
 	public void run() {

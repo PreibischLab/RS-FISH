ad7821c16b6411efbadde4413308c91fb610eb5e
milkyklim <klim.kolyvanov@gmail.com>
2018-02-20 21:22:05 +0100

    added class to store default values

---
+++ src/main/java/paper/test/radialsymmetry/InputParams.java
@@ -0,0 +1,14 @@
+package paper.test.radialsymmetry;
+
+
+public class InputParams {
+	// stores input parameters for images and csv files
+	public static String path = "/Users/kkolyva/Desktop/";
+	public static String imgName = "test-2D-image";
+	public static String posName = "test-2D-pos";
+	public static long [] dims = new long[] {512, 512};
+	public static double [] sigma = new double[] {3, 3}; 
+	public static long numSpots = 800;
+	public static int seed = 42;
+	public static boolean padding = false;
+}

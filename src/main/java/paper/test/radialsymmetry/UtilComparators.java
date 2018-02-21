f0844ac627f1ee76f5ca49b2aa6970395ff533ae
milkyklim <klim.kolyvanov@gmail.com>
2018-02-21 15:47:59 +0100

    added new comparator

--- src/main/java/paper/test/radialsymmetry/UtilComparators.java
+++ src/main/java/paper/test/radialsymmetry/UtilComparators.java
@@ -6,9 +6,6 @@ import fit.Spot;
 
 public class UtilComparators {
 	// contains useful comparators 
-	
-	// TODO: I have to write my own comparator
-	
 	public class doubleComparator implements Comparator<double[]>{
 		@Override
 		public int compare(double[] e1, double[] e2) {
@@ -24,16 +21,44 @@ public class UtilComparators {
 			}
 			return result;
 		}
-		
 	}
-	
+
 	public class spotComparator implements Comparator<Spot>{
 		@Override
 		public int compare(Spot e1, Spot e2) {
-			// TODO Auto-generated method stub
-			return 0;
+			int result = 0; // same 
+			for (int j = 0; j < e1.numDimensions(); j++) {
+				if (e1.getDoublePosition(j) < e2.getDoublePosition(j)) {
+					result = -1;
+					break;
+				} else if (e1.getDoublePosition(j) > e2.getDoublePosition(j)) {
+					result = 1;
+					break;
+				}
+			}
+			return result;
 		}
+	}
 
+	public class approxComparator implements Comparator<double[]>{
+		private double eps = 0;
+		public approxComparator(double eps) {
+			this.eps = eps;
+		}
+		@Override
+		public int compare(double[] e1, double[] e2) {
+			int result = 0; // same 
+			if (dist(e1, e2) > eps) 
+				result = 1;
+			return result;
+		}
 	}
 	
+	public double dist(double[] e1, double[] e2){
+		double dist = 0;
+		for (int j = 0; j < e1.length; j++)
+			dist += (e1[j] - e2[j])*(e1[j] - e2[j]);
+		return Math.sqrt(dist);
+	}
+
 }

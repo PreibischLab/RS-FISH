7f8ab8032b96d61b7da10217bafc126addd79871
milkyklim <klim.kolyvanov@gmail.com>
2018-03-15 17:36:51 +0100

    added function to resave the image from xy(c) to xy(z)

--- src/main/java/process/radialsymmetry/cluster/IOFunctions.java
+++ src/main/java/process/radialsymmetry/cluster/IOFunctions.java
@@ -4,12 +4,21 @@ import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.real.FloatType;
 
+import ij.ImagePlus;
 import ij.io.FileSaver;
 
 public class IOFunctions {
 	public static void saveResult(Img <FloatType> img, String path) {
 		// saving part
 		FileSaver fs = new FileSaver(ImageJFunctions.wrap(img, ""));
+		fs.saveAsTiff(path);
+	}
+	
+	public static void saveResultXyz(Img <FloatType> img, String path) {
+		// saving part
+		ImagePlus imp = ImageJFunctions.wrap(img, "");
+		imp.setDimensions(1, imp.getNSlices(), 1);
+		FileSaver fs = new FileSaver(imp);
 		fs.saveAsTiff(path);
 	} 
 }

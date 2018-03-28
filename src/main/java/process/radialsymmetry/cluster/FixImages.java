7904df782e0e5117572d988a93874770f462f990
milkyklim <klim.kolyvanov@gmail.com>
2018-03-28 10:30:05 +0200

    added function to copy the roi from one image to another

--- src/main/java/process/radialsymmetry/cluster/FixImages.java
+++ src/main/java/process/radialsymmetry/cluster/FixImages.java
@@ -1,33 +1,24 @@
 package process.radialsymmetry.cluster;
 
 import java.io.File;
-import java.io.FileReader;
 import java.util.ArrayList;
 
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.real.FloatType;
 
+import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.Roi;
 import util.ImgLib2Util;
-import util.opencsv.CSVReader;
 
 public class FixImages {
-	
-	
-	
 	// class to fix the number of slices in the stacks 
 	// and add the roi that are gone because of channel
 	// splitting 
-	// folder - folder with stacks to reshape + add roi
-	public static ArrayList<File> readImages(File iFolder) {
-		ArrayList<File> paths = IOFunctions.readFolder(iFolder, ".tif");
-		for (File imgPath : paths) {
-			// File outputPath = new File(imgPath.getAbsolutePath().substring(0, imgPath.getAbsolutePath().length() - 4));
-			reshapeImage(imgPath);
-		}
-		return paths;
-	}
+	
+	private static boolean debug = false;
 
 	// here process only one image
 	public static void reshapeImage(File imgPath) {
@@ -41,66 +32,58 @@ public class FixImages {
 		// TODO: uncomment when the code is working to have proper saving 
 		// IOFunctions.saveResult(imp, "");
 	}
-
-	// 
-	public static void checkThatTheImageIsSmFish() {
-		// TODO:
-		// iterate over xls 
-		// iterate over the columns 
-		// if column is smfish add the file to the process file set 
-		// otherwise ignore
-
-		// TODO: always check the input folder 
-		File path = new File("/Users/kkolyva/Desktop/smFISH-database/SEA-12-Table 1.csv");
-		
-		// files that we will consider in the end
-		// ArrayList <String> files = new ArrayList<>();
-		// TODO: add the length wave check here, too
-		ArrayList <ImageData> imageData = new ArrayList<>(); 
-		
-		CSVReader reader = null;
-		int nColumns = 24;
-		String[] nextLine = new String [nColumns];
-		// columns: 3, 6, 9, 12, 15 
-		int [] lambdaIndices = new int[] {2, 5, 8, 11, 14};
-		int [] stainIndices = new int[] {3, 6, 9, 12, 15};
-		// index for the column with the new name
-		int newFilenameIndex = 23;
-
-		try {
-			int toSkip = 1; 
-			reader = new CSVReader(new FileReader(path), ',', CSVReader.DEFAULT_QUOTE_CHARACTER, toSkip);
-			// while there are rows in the file
-			while ((nextLine = reader.readNext()) != null) {
-				// iterate over the row; that is 25 elements long
-				for (int j = 0; j < stainIndices.length; j++) {
-					// 
-					if (nextLine[stainIndices[j]].equals("FISH") && conditionalPick()) {
-						// TODO: Check the naming for the files!
-						// files.add(new String(nextLine[newFilenameIndex] + "-C" + j));
-						
-						int lambda = Integer.parseInt(nextLine[lambdaIndices[j]]);
-						String filename = nextLine[newFilenameIndex] + "-C" + j;
-						
-						imageData.add(new ImageData(lambda, filename));
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		for (ImageData id : imageData)
-			System.out.println(id.getLambda() + " " + id.getFilename());
-	}
 	
-	// drop bad quality images
-	public static boolean conditionalPick() {
-		// true -> good quality
-		return true;
+	public static void addRoi(File imgPath, ArrayList<File> roiPaths, File oFolder) {
+		String imageName = imgPath.getName().substring(3);
+		for (File imageRoi : roiPaths) {
+			if (imageRoi.getName().equalsIgnoreCase(imageName)){
+				System.out.println("fixed: " + imgPath.getName());
+				// open both images
+				ImagePlus impNoRoi = IJ.openImage(imgPath.getAbsolutePath());
+				ImagePlus impRoi = IJ.openImage(imageRoi.getAbsolutePath());
+				// grab and copy roi
+				Roi roi = impRoi.getRoi();
+				impNoRoi.setRoi(roi);
+				
+				if (debug) {
+					impRoi.show();
+					impNoRoi.show();
+				}
+				
+				IJ.save(impNoRoi, oFolder.getAbsolutePath() + "/" + imgPath.getName());
+				
+				break; // iterate till the first hit 
+			}
+		}
+	}
+
+	public static void runFixImages(File iFolder, File roiFolder, File oFolder) {
+		ArrayList<File> paths = IOFunctions.readFolder(iFolder, ".tif");
+		ArrayList<File> roiPaths = IOFunctions.readFolder(roiFolder, ".tif");
+		for (File imgPath : paths) {
+			// File outputPath = new File(imgPath.getAbsolutePath().substring(0, imgPath.getAbsolutePath().length() - 4));
+			// reshapeImage(imgPath);
+			addRoi(imgPath, roiPaths, oFolder);
+		}
 	}
 
 	public static void main(String [] args) {
-		checkThatTheImageIsSmFish();
+		// TODO: move this part from here 
+		new ImageJ();
+		// no roi folder:
+		File iFolder = new File("/Volumes/1TB/2018-03-20-laura-radial-symmetry-numbers/SEA-12-channels-preprocessed-1");
+		// roi folder:
+		File roiFolder = new File("/Volumes/1TB/2018-03-20-laura-radial-symmetry-numbers/SEA-12");
+		// roi folder:
+		File oFolder = new File("/Volumes/1TB/2018-03-20-laura-radial-symmetry-numbers/SEA-12-channels-preprocessed-2");
+		
+		// ArrayList<File> list = new ArrayList<>();
+		// list.add(folderRoi);
+		
+		
+		runFixImages(iFolder, roiFolder, oFolder);
+		// addRoi(folderNoRoi, list, new File (""));
+		
+		System.out.println("DONE!");
 	}
 }

f0aa8c90a1cc61e42f58e205e6747b1220beff7d
milkyklim <klim.kolyvanov@gmail.com>
2018-04-03 09:34:19 +0200

    added the prefix for the folders/paths

--- src/main/java/process/radialsymmetry/cluster/FullProcess.java
+++ src/main/java/process/radialsymmetry/cluster/FullProcess.java
@@ -5,14 +5,16 @@ import java.io.File;
 public class FullProcess {
 
 	public static void runFullProcess() {
+		// TODO: make this one more general
+		String prefix = "/media/milkyklim/1TB";
 		// path to the images with roi
-		File pathImagesRoi = new File("/Volumes/1TB/2018-03-29-laura-radial-symmetry-numbers/SEA-12");
+		File pathImagesRoi = new File(prefix +"/2018-03-29-laura-radial-symmetry-numbers/SEA-12");
 		// path to separate channel images
-		File pathImages = new File("/Volumes/1TB/2018-03-29-laura-radial-symmetry-numbers/SEA-12-channels-correct");
+		File pathImages = new File(prefix +"/2018-03-29-laura-radial-symmetry-numbers/SEA-12-channels-correct");
 		// path to the database with the images
-		File pathDatabase = new File("/Volumes/1TB/2018-03-29-laura-radial-symmetry-numbers/smFISH-database/SEA-12-Table 1.csv");
+		File pathDatabase = new File(prefix +"/2018-03-29-laura-radial-symmetry-numbers/smFISH-database/SEA-12-Table 1.csv");
 		// path to the median filtered images that we save
-		File pathImagesMedian = new File("/Volumes/1TB/2018-04-03-laura-images-processing");
+		File pathImagesMedian = new File(prefix +"/2018-04-03-laura-images-processing");
 		// path to save the .csv files with the results
 		File pathResultCsv = new File("");
 		

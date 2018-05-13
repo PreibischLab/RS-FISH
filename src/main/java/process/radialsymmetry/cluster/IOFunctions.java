050e1aa18afca54d0adc62526015764cff89aaac
milkyklim <klim.kolyvanov@gmail.com>
2018-05-13 19:24:16 +0200

    added .duplicate() otherwise images are not saved properly

--- src/main/java/process/radialsymmetry/cluster/IOFunctions.java
+++ src/main/java/process/radialsymmetry/cluster/IOFunctions.java
@@ -7,14 +7,14 @@ import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.real.FloatType;
 
+import ij.IJ;
 import ij.ImagePlus;
 import ij.io.FileSaver;
 
 public class IOFunctions {
 	public static void saveResult(Img <FloatType> img, String path) {
 		// saving part
-		FileSaver fs = new FileSaver(ImageJFunctions.wrap(img, ""));
-		fs.saveAsTiff(path);
+		IJ.saveAsTiff(ImageJFunctions.wrap(img, "").duplicate(), path);
 	}
 	
 	public static void saveResultXyz(Img <FloatType> img, String path) {

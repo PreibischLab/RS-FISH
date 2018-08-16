13bd17973bef3b1855bd9271785aa267ad316757
milkyklim <klim.kolyvanov@gmail.com>
2018-08-16 11:24:53 +0200

    Utils update:
    - class to read the spots from the csv;
    - class with the comparators.

---
+++ src/main/java/radial/symmetry/utils/IOUtils.java
@@ -0,0 +1,47 @@
+package radial.symmetry.utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import net.imglib2.RealPoint;
+
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+
+import util.opencsv.CSVReader;
+
+public class IOUtils {
+	public static int getNumDimensions(File filepath, char separator) {
+		int numDimensions = 0;
+		try {
+			CSVReader reader = new CSVReader(new FileReader(filepath), separator); // we don't care about the delimiter
+			String[] nextLine = reader.readNext();
+			numDimensions = nextLine.length; 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return numDimensions;
+	}
+	
+	// IMP: this reader assumes that images are 3D-stacks {x, y, z}
+	public static ArrayList<RealPoint> readPositionsFromCSV(File filepath, char separator){
+		ArrayList<RealPoint> peaks = new ArrayList<>();
+		String[] nextLine;
+		int numDimensions = getNumDimensions(filepath, separator) - 2;
+		try {
+			CSVReader reader = new CSVReader(new FileReader(filepath), separator); // figure the delimeter yourself
+			while ((nextLine = reader.readNext()) != null) {
+				double [] pos = new double[numDimensions];
+				for (int d = 0; d < numDimensions; d++)
+					pos[d] = Double.parseDouble(nextLine[d + 1]);
+				peaks.add(new RealPoint(pos));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return peaks;
+	}
+}

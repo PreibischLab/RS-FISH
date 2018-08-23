3575a6e9519cc39324e4f13ec3507206ab063327
milkyklim <klim.kolyvanov@gmail.com>
2018-08-23 19:19:40 +0200

    added example for processing multiple images

--- src/main/java/test/TestProcessIntrons.java
+++ src/main/java/test/TestProcessIntrons.java
@@ -6,8 +6,31 @@ import java.nio.file.Paths;
 import scripts.radial.symmetry.process.ProcessIntrons;
 
 public class TestProcessIntrons {
+	
+	
+	public static void testProcessImageSet(){
+		String root = "/Users/kkolyva/Desktop/2018-07-31-09-53-32-N2-all-results-together/test";
+		
+		int nFiles = 4;
+		
+		String[] intronImageFilename = new String []{"C2-N2_395.tif", "C3-N2_447.tif","C3-N2_459.tif","C3-N2_542.tif"};
+		String[] exonFilename = new String []{"C1-N2_395.csv", "C1-N2_447.csv","C1-N2_459.csv","C1-N2_542.csv"};
+		String[] intronFilename = new String []{"C2-N2_395.csv", "C3-N2_447.csv","C3-N2_459.csv","C3-N2_542.csv"};
+		
+		for (int j = 0; j < nFiles; j++) {
+			File intronImagePath = Paths.get(root, "median", intronImageFilename[j]).toFile();
+			File exonPath = Paths.get(root, "csv-2", exonFilename[j]).toFile();
+			File intronPath = Paths.get(root, "csv-dapi-intron", intronFilename[j]).toFile();
+			
+			ProcessIntrons.processImage(intronImagePath, exonPath, intronPath);
+		}
+		
+		System.out.println("DOGE!");
+	}
+	
 
 	public static void testProcessImage() {
+		
 		String root = "/Users/kkolyva/Desktop/2018-07-31-09-53-32-N2-all-results-together/test";
 		String intronImageFilename = "C2-N2_395.tif";
 		String exonFilename = "C1-N2_395.csv";
@@ -16,8 +39,8 @@ public class TestProcessIntrons {
 		// IOUtils.checkPaths(...)
 
 		File intronImagePath = Paths.get(root, "median", intronImageFilename).toFile();
-		File exonPath = Paths.get(root, exonFilename).toFile();
-		File intronPath = Paths.get(root, intronFilename).toFile();
+		File exonPath = Paths.get(root, "csv-2", exonFilename).toFile();
+		File intronPath = Paths.get(root, "csv-dapi-intron", intronFilename).toFile();
 
 		ProcessIntrons.processImage(intronImagePath, exonPath, intronPath);
 
@@ -25,6 +48,6 @@ public class TestProcessIntrons {
 	}
 	
 	public static void main(String[] args){
-		testProcessImage();
+		testProcessImageSet();
 	}
 }

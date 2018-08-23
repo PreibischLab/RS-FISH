7e501564939ab5239b35f1f5c991066ad81e37a9
milkyklim <klim.kolyvanov@gmail.com>
2018-08-23 13:53:47 +0200

    added test function for intron processing; rewrote the reading part of the db

--- src/main/java/test/TestProcessIntrons.java
+++ src/main/java/test/TestProcessIntrons.java
@@ -1,11 +1,30 @@
 package test;
 
+import java.io.File;
+import java.nio.file.Paths;
+
+import scripts.radial.symmetry.process.ProcessIntrons;
 
 public class TestProcessIntrons {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void testProcessImage() {
+		String root = "/Users/kkolyva/Desktop/2018-07-31-09-53-32-N2-all-results-together/test";
+		String intronImageFilename = "C2-N2_395.tif";
+		String exonFilename = "C1-N2_395.csv";
+		String intronFilename = "C2-N2_395.csv";
 
+		// IOUtils.checkPaths(...)
+
+		File intronImagePath = Paths.get(root, "median", intronImageFilename).toFile();
+		File exonPath = Paths.get(root, exonFilename).toFile();
+		File intronPath = Paths.get(root, intronFilename).toFile();
+
+		ProcessIntrons.processImage(intronImagePath, exonPath, intronPath);
+
+		System.out.println("DOGE!");
 	}
-
+	
+	public static void main(String[] args){
+		testProcessImage();
+	}
 }

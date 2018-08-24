e46c718589af26ca90a0872538a36b780ddb4e36
milkyklim <klim.kolyvanov@gmail.com>
2018-08-24 17:57:13 +0200

    new info function

--- src/main/java/util/NotSoUsefulOutput.java
+++ src/main/java/util/NotSoUsefulOutput.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.util.Collection;
+
+import cluster.radial.symmetry.process.ImageData;
 
 public class NotSoUsefulOutput {
 	public static String toProgressString(long current, long total, String name) {
@@ -8,5 +11,12 @@ public class NotSoUsefulOutput {
 	
 	public static String toComplaintString(String className, String name) {
 		return String.format("%s: %s is missing", className, name);
+	}
+	
+	public static void printImageDataParameters(Collection<ImageData> imageData) {
+		for (ImageData id : imageData) {
+			String tmp = id.getDefects() ? "" : "out"; 
+			System.out.println(String.format("Wavelength: %d, Type: %s, filename %s with%s defects", id.getLambda(), id.getType(), id.getFilename(), tmp));
+		}
 	}
 }
